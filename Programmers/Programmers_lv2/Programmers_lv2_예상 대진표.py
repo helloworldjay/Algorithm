e24878784dc9672044d80c447e65b9eb63cd0adb
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2021-01-10 18:13:22 +0900

    feat: solve Programmers_lv2_예상 대진표.py

--- Programmers/Programmers_lv2/Programmers_lv2_예상 대진표.py
+++ Programmers/Programmers_lv2/Programmers_lv2_예상 대진표.py
@@ -1,10 +1,10 @@
-def fibo(n):
-    dp = [0]*(n+1)
-    if n < 3:
-        return 1
-    if dp[n] != 0:
-        return dp[n]
-    dp[n] = fibo(n-1) + fibo(n-2)
-    return dp[n]
+def solution(n,a,b):
+    cnt = 0
+    a -= 1
+    b -= 1
+    while a != b:
+        cnt += 1
+        a //= 2
+        b //= 2
+    return cnt
 
-print(fibo(5))
