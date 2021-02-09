dd1fb88b06f6a70360470f659077ec21e1c0dec0
helloworldjay <sj.seungjin.baek@gmail.com>
2021-02-09 18:07:04 +0900

    feat: solve LeetCode Two Sum

--- LeetCode/7장/두 수의 합.py
+++ LeetCode/7장/두 수의 합.py
@@ -9,3 +9,19 @@ class Solution:
             if target - nums[i] in nums_dict:
                 return [nums_dict[target], i]
             nums_dict[nums[i]] = i
+
+    # two pointer 사용해보기
+    def twoPointerSum(self, nums: List[int], target: int) -> List[int]:
+        index_num = []
+        for index, num in enumerate(nums):
+            index_num.append((index, num))
+        index_num.sort(key=lambda x: (x[1], x[0]))
+        left, right = 0, len(nums) - 1
+        while left != right:
+            if index_num[left][1] + index_num[right][1] == target:
+                return [index_num[left][0], index_num[left][1]]
+            elif index_num[left][1] + index_num[right][1] > target:
+                right -= 1
+            else:
+                left -= 1
+        return [-1, -1] # in case of no answer
