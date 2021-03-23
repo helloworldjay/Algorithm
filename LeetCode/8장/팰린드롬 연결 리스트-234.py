155017789482bca6a7f2c6c67fd7cec0c70e87f2
helloworldjay <sj.seungjin.baek@gmail.com>
2021-03-23 22:01:37 +0900

    change LeetCode Problem

--- LeetCode/8장/팰린드롬 연결 리스트-234.py
+++ LeetCode/8장/팰린드롬 연결 리스트-234.py
@@ -2,27 +2,42 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-from collections import deque
+# from collections import deque
+#
+#
+# # 나의 풀이
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         head_list = []
+#         while head:
+#             head_list.append(head.val)
+#             head = head.next
+#         return head_list == head_list[::-1]
+#
+# # 책에 나온 풀이
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         num_list = deque([])
+#         while head is not None: # head.val is not None은 안됨
+#             num_list.append(head.val)
+#             head = head.next
+#         while len(num_list) > 1:
+#             if num_list.popleft() == num_list.pop():
+#                 continue
+#             return False
+#         return True
 
-
-# 나의 풀이
+# 런너 기법
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        head_list = []
-        while head:
-            head_list.append(head.val)
-            head = head.next
-        return head_list == head_list[::-1]
-
-# 책에 나온 풀이
-class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        num_list = deque([])
-        while head is not None: # head.val is not None은 안됨
-            num_list.append(head.val)
-            head = head.next
-        while len(num_list) > 1:
-            if num_list.popleft() == num_list.pop():
-                continue
-            return False
-        return True
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+            print(rev)
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
