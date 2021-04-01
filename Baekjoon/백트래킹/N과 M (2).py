89442b8900fd46b4de28db7e6080386d10eec5a6
helloworldjay <sj.seungjin.baek@gmail.com>
2021-04-01 21:30:17 +0900

    solve N과M(2) in Back Tracking

--- Baekjoon/백트래킹/N과 M (2).py
+++ Baekjoon/백트래킹/N과 M (2).py
@@ -1,1 +1,17 @@
-
+from sys import stdin
+input = stdin.readline
+N, M = map(int, input().split())
+partial_sequence = []
+visited = [False] * N
+def backtracking(depth):
+    if depth == M:
+        print(' '.join(map(str, partial_sequence)))
+    else:
+        for i in range(N):
+            if not visited[i] and (not partial_sequence or partial_sequence[-1] < (i+1)):
+                visited[i] = True
+                partial_sequence.append(i+1)
+                backtracking(depth+1)
+                partial_sequence.pop()
+                visited[i] = False
+backtracking(0)
