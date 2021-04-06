c239d2c37550c86c88c8c30824b4f77d07bb1a63
helloworldjay <sj.seungjin.baek@gmail.com>
2021-04-06 17:29:04 +0900

    fix: change code for 좌표 압축 in Sort

--- Baekjoon/정렬/좌표 압축.py
+++ Baekjoon/정렬/좌표 압축.py
@@ -3,8 +3,23 @@ from sys import stdin
 input = stdin.readline
 N = int(input())
 x_list = list(map(int, input().split()))
-sorted_list = sorted(x_list)
+sorted_list = sorted(list(set(x_list)))
+sorted_dict = {value : index for index, value in enumerate(sorted_list)}
 result = [0 for _ in range(N)]
 for i in range(N):
-    result[i] = sorted_list.index(x_list[i])
+    result[i] = sorted_dict[x_list[i]]
 print(' '.join(map(str, result)))
+
+
+
+
+# from sys import stdin
+#
+# input = stdin.readline
+# N = int(input())
+# x_list = list(map(int, input().split()))
+# sorted_list = sorted(x_list)
+# result = [0 for _ in range(N)]
+# for i in range(N):
+#     result[i] = sorted_list.index(x_list[i])
+# print(' '.join(map(str, result)))
