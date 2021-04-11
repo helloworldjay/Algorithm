b31ddbf9a5695a1e90190de21dc0647e1d870cdc
helloworldjay <sj.seungjin.baek@gmail.com>
2021-04-11 22:29:21 +0900

    tried AC in Deque

--- Baekjoon/큐/AC.py
+++ Baekjoon/큐/AC.py
@@ -1,22 +1,51 @@
 from sys import stdin
 from collections import deque
 input = stdin.readline
-testcase = int(input())
-for _ in range(testcase):
-    orders = input().strip().replace("RR", "")
-    orders = deque(list(orders))
+T = int(input())
+result = []
+for _ in range(T):
+    reversed = False
+    function = input().strip()
     n = int(input())
-    integer_list = deque(list(input().strip().lstrip('[').rstrip(']').split(',')))
-    isCheck = True
-    while orders:
-        order = orders.popleft()
-        if order == "R":
-            integer_list.reverse()
-            continue
-        else:
-            if integer_list:
-                integer_list.popleft()
+    array = deque(list(input().lstrip('[').rstrip(']\n').split(',')))
+    if len(array) < function.count('D') or array[0] == '':
+        result.append("error")
+        continue
+    function = function.replace("RR", "")
+    for one_function in function:
+        if one_function == "R":
+            reversed = False if reversed else True
+        elif one_function == "D":
+            if reversed:
+                array.pop()
             else:
-                isCheck = False
-                break
-    print("["+",".join(integer_list)+"]") if isCheck and integer_list else print("error")
+                array.popleft()
+    if reversed:
+        array.reverse()
+    result.append(f"[{','.join(array)}]")
+print('\n'.join(result))
+
+
+
+# from sys import stdin
+# from collections import deque
+# input = stdin.readline
+# testcase = int(input())
+# for _ in range(testcase):
+#     orders = input().strip().replace("RR", "")
+#     orders = deque(list(orders))
+#     n = int(input())
+#     integer_list = deque(list(input().strip().lstrip('[').rstrip(']').split(',')))
+#     isCheck = True
+#     while orders:
+#         order = orders.popleft()
+#         if order == "R":
+#             integer_list.reverse()
+#             continue
+#         else:
+#             if integer_list:
+#                 integer_list.popleft()
+#             else:
+#                 isCheck = False
+#                 break
+#     print("["+",".join(integer_list)+"]") if isCheck and integer_list else print("error")
