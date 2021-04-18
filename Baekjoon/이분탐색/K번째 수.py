c087b8baf67cdd08d2770eda2631c3531ed3154b
helloworldjay <sj.seungjin.baek@gmail.com>
2021-04-18 22:54:35 +0900

    solve K번째수 in 이분탐색

---
+++ Baekjoon/이분탐색/K번째 수.py
@@ -0,0 +1,15 @@
+from sys import stdin
+input = stdin.readline
+N, k = int(input()), int(input())
+start, end = 1, k
+while start <= end:
+    mid = (start+end) // 2
+    temp = 0
+    for i in range(1, N+1):
+        temp += min(mid//i, N)
+    if temp >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+print(answer)
