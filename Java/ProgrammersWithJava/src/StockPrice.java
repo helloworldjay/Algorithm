0900c915b2ca60e7e4a056b9061d307303f8e98c
helloworldjay <sj.seungjin.baek@gmail.com>
2021-06-27 20:41:35 +0900

    solve 2problems with java

--- Java/ProgrammersWithJava/src/StockPrice.java
+++ Java/ProgrammersWithJava/src/StockPrice.java
@@ -1,26 +1,27 @@
 import java.util.*;
 public class StockPrice {
     public int[] solution(int[] prices) {
-        Stack<int[]> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         int[] result = new int[prices.length];
-        stack.add(new int[]{0, prices[0]});
+        stack.add(0);
         for (int i = 1; i < prices.length; i++) {
             while (!stack.isEmpty()) {
-                if (stack.peek()[1] > prices[i]) {
-                    int[] element = stack.pop();
-                    result[element[0]] = i - element[0];
+                if (prices[stack.peek()] > prices[i]) {
+                    int element = stack.pop();
+                    result[element] = i - element;
                 } else {
                     break;
                 }
             }
             if (i == prices.length-1) {
                 while (!stack.isEmpty()) {
-                    int[] element = stack.pop();
-                    result[element[0]] = prices.length - element[0] - 1;
+                    int element = stack.pop();
+                    result[element] = prices.length - element - 1;
                 }
             }
-            stack.add(new int[]{i, prices[i]});
+            stack.add(i);
         }
         return result;
+        // 	실행한 결괏값 [4,0,2,1,0]이(가) 기댓값 [4,3,1,1,0]와(과) 다릅니다.
     }
 }
--- Java/ProgrammersWithJava/src/TruckPassingByBridge.java
+++ Java/ProgrammersWithJava/src/TruckPassingByBridge.java
@@ -2,18 +2,16 @@ import java.util.*;
 
 public class TruckPassingByBridge {
     public static void main(String[] args) {
-        solution();
+//        solution(2, 10, new int[]{7,4,6,8});
     }
 
-    /*public static int solution(int*/ bridge_length, int weight, int[] truck_weights) {
-    /*    Queue<Integer> passingTruc*/ks = new LinkedList<>();
-    /*    Queue<Integer> passedTruck*/s = new LinkedList<>();
-    /*    Queue<Object> truckWeights*/ = new LinkedList<>(Arrays.asList(truck_weights));
-    /*    int sec = 0;*/
-    /*    System.out.println(truckWe*/ights.peek());s
-    /*    while (passedTrucks.size()*/ != truck_weights.length) {
-//            if ()
-    /*    }*/
-    /*    return sec;*/
-    /*}*/
+//    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+//        Queue<int[]> passingTrucks = new LinkedList<>();
+//        Queue<int[]> passedTrucks = new LinkedList<>();
+////        Queue<Object> truckWeights = new LinkedList<>(Arrays.asList(truck_weights));
+//        Queue<Integer> truckWeights = new LinkedList<>();
+//        Arrays.stream(truck_weights).forEach(x -> truckWeights.add(x));
+//
+//        return sec;
+//    }
 }
