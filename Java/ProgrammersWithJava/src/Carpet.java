207916125286b1732b511ca6ce054231310423c4
helloworldjay <sj.seungjin.baek@gmail.com>
2021-06-30 23:34:02 +0900

    solve 2 problems with java

--- Java/ProgrammersWithJava/src/Carpet.java
+++ Java/ProgrammersWithJava/src/Carpet.java
@@ -1,3 +1,5 @@
 public class Carpet {
-
+    public int[] solution(int brown, int yellow) {
+        return new int[] { (int) (((brown+4)/2)+Math.sqrt((((brown+4)/2)*((brown+4)/2))-4*(yellow+brown)))/2, (int) (((brown+4)/2)-Math.sqrt((((brown+4)/2)*((brown+4)/2))-4*(yellow+brown)))/2};
+    }
 }
